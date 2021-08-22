887164a6f6a66fe51fdd5525023f330b3973400a
kmatsui058 <kmatsui058@gmail.com>
2021-08-22 19:27:26 +0900

    formatter

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,29 @@
 <template>
-  <div :hoge="hoge">{{contents}}{{axles}}</div>
+  <div :hoge="hoge">{{ contents }}{{ axles }}</div>
 </template>
 
 <script lang="ts">
 import { IContentDocument } from '@nuxt/content/types/content'
-import { defineComponent, Ref, ref, useContext, useFetch, } from '@nuxtjs/composition-api'
-import {mymoduleStore} from '~/store'
+import {
+  defineComponent,
+  Ref,
+  ref,
+  useContext,
+  useFetch,
+} from '@nuxtjs/composition-api'
+import { mymoduleStore } from '~/store'
 
 export default defineComponent({
   name: 'IndexPageComponent',
   setup() {
     const context = useContext()
     const axles = mymoduleStore.axles
-    const contents: Ref<null | IContentDocument | IContentDocument[]> = ref(null)
-    useFetch(async ()=>{
-         contents.value = await context.$content().fetch()
-
+    const contents: Ref<null | IContentDocument | IContentDocument[]> =
+      ref(null)
+    useFetch(async () => {
+      contents.value = await context.$content().fetch()
     })
-    return {contents, axles}
+    return { contents, axles }
   },
 })
 </script>
